dba728e2376f0f96419c899cd74436d84467659b
Luan Gustavo <lgns@cin.ufpe.br>
2026-05-10 17:28:42 -0300

    termina 06, 08 (sql)

--- sql/dql/sql/06. SELECT-FROM-WHERE.sql
+++ sql/dql/sql/06. SELECT-FROM-WHERE.sql
@@ -2,5 +2,14 @@
  * SELECT-FROM-WHERE
  *
  * Usos:
- * 1.
+ * 1. Selecione o nome e salário de todos os cargos registrados.
+ * 2. Selecione o cargo e o turno de todo funcionário ativo.
+ * 3. Selecione o componente, tipo sanguíneo e validade de 
+ *    todos os hemocomponentes com volume maior que 200.
  */
+
+SELECT nome_cargo, salario FROM Cargo;
+
+SELECT cargo, turno FROM Funcionario WHERE status = 'Ativo';
+
+SELECT componente, tipo_sanguineo, validade FROM HEMOCOMPONENTE WHERE volume > 200;
--- sql/dql/sql/08. IN.sql
+++ sql/dql/sql/08. IN.sql
@@ -2,10 +2,29 @@
  * IN
  *
  * Usos:
- * 1. Realiza uma consulta que mostre o CPF, a naturalidade (estado e município) e o 
+ * 1. Realiza uma consulta que mostra o CPF, a naturalidade (estado e município) e o 
  *    tipo sanguíneo dos doadores que possuem os tipos sanguíneos 'A+', 'B+' ou 'O+'.
+ * 2. Realiza uma consulta que mostra o nome e o municipio de nascimento de todos os
+ *    doadores que nasceram em Alagoas ou Sergipe.
+ * 3. Seleciona o cnpj de todas as agências e hemocentros que estão localizados
+ *    ou na Avenida Central, ou na Avenida Caxangá, ou na Avenida Conde da Boa Vista. 
  */
 
 SELECT cpf, naturalidade_municipio, naturalidade_estado, tipo_sanguineo
 FROM Doador
 WHERE tipo_sanguineo IN ('A+', 'B+', 'O+');
+
+SELECT P.nome_principal, D.naturalidade_municipio
+FROM Doador D INNER JOIN Pessoa P ON P.cpf = D.cpf
+WHERE D.naturalidade_estado IN ('AL', 'SE');
+
+SELECT cnpj
+FROM (
+    SELECT cnpj, cep FROM Hemocentro H
+    UNION
+    SELECT cnpj, cep FROM Agencia A
+)
+WHERE cep IN (
+    SELECT cep FROM Endereco
+    WHERE logradouro IN ('Avenida Central', 'Avenida Caxangá', 'Avenida Conde da Boa Vista')
+);
