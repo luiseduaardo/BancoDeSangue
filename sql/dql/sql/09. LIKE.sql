8789d7570f4901bb34370bd6082565ca6749961f
Luan Gustavo <lgns@cin.ufpe.br>
2026-05-10 17:41:28 -0300

    termina 09 (sql)

--- sql/dql/sql/09. LIKE.sql
+++ sql/dql/sql/09. LIKE.sql
@@ -2,10 +2,21 @@
  * LIKE
  *
  * Usos:
- * 1. Realize uma consulta que mostre o nome principal, o sobrenome e o volume da 
+ * 1. Realiza uma consulta que mostra o nome principal, o turno e o status
+ *    de todo funcionário cujo cargo tenha a palavra "médico".
+ * 2. Seleciona o nome de todo hemocentro que se localiza em uma avenida.
+ * 3. Realiza uma consulta que mostra o nome principal, o sobrenome e o volume da 
  *    coleta dos doadores que doaram um volume entre 400 e 450 e cujo nome principal
  *    termine com a letra "a".
  */
+
+SELECT P.nome_principal, F.turno, F.status
+FROM Pessoa P INNER JOIN Funcionario F ON P.cpf = F.cpf
+WHERE LOWER(F.cargo) LIKE '%médico%';
+
+SELECT H.nome
+FROM Hemocentro H INNER JOIN Endereco E ON H.cep = E.cep
+WHERE E.logradouro LIKE 'Avenida%';
 
 SELECT P.nome_principal, P.sobrenome, DC.volume_coleta
 FROM Pessoa P INNER JOIN DOADOR D ON P.cpf = D.cpf
