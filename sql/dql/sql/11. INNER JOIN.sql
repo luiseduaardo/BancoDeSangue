10a9bf385449a4a0d7784459a6db49e39eff92a3
Luan Gustavo <lgns@cin.ufpe.br>
2026-05-10 17:59:59 -0300

    termina 11 (sql)

--- sql/dql/sql/11. INNER JOIN.sql
+++ sql/dql/sql/11. INNER JOIN.sql
@@ -5,6 +5,8 @@
  * 1. Imprime o nome completo e endereço dos doadores registrados.
  * 2. Imprime as pressões sanguíneas aferidas, as medicações (e suas doses) e
  *    as doenças registradas (e sua cronicidade) de cada triagem, junto com seu código.
+ * 3. Imprime o nome do hemocentro de origem, o nome do hemocentro de chegada e as
+ *    informações do primeiro hemocomponente a ser unicamente transferido de centro a centro.
  */
 
 SELECT (P.nome_principal || ' ' || P.sobrenome)
@@ -14,3 +16,13 @@ SELECT T.codigo, T.pressao_sistolica, T.pressao_diastolica,
        D.doenca, D.cronica, M.medicamento, M.dose
 FROM Triagem T INNER JOIN Medicacoes_Triagem M ON T.codigo = M.codigo_triagem
      INNER JOIN Doencas_Triagem D ON T.codigo = D.codigo_triagem;
+
+SELECT H1.nome AS origem, H2.nome AS destino, C.componente, C.tipo_sanguineo, C.volume
+FROM Hemocentro H1 INNER JOIN Lote L ON L.hemocentro_origem = H1.cnpj
+     INNER JOIN Hemocentro H2 ON L.hemocentro_transferido = H2.cnpj
+     INNER JOIN Hemocomponente C ON L.codigo_hemocomponente = C.codigo
+WHERE C.codigo IN (
+     SELECT MIN(codigo_hemocomponente)
+     FROM Lote
+     WHERE agencia_transferida IS NULL and hemocentro_transferido IS NOT NULL
+);
