b593e5c356e761b95aa3f48364bf24098de72b1f
lecs2 <cavalcantesantosluiseduardo@gmail.com>
2026-05-11 21:56:38 -0300

    adiciona trigger de comando para aptidão

---
+++ sql/dql/pl/AuditLote.sql
@@ -0,0 +1,32 @@
+DROP TABLE AuditoriaLote;
+DROP SEQUENCE id_audit;
+
+CREATE TABLE AuditoriaLote (
+    id INTEGER,
+    usuario VARCHAR2 (50),
+    data_alteracao TIMESTAMP,
+    operacao VARCHAR2 (50),
+
+    CONSTRAINT auditorialote_pkey PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE id_audit
+INCREMENT BY 1 START WITH 1;
+
+CREATE OR REPLACE TRIGGER AuditLote
+AFTER DELETE OR UPDATE OR INSERT ON Lote
+DECLARE
+    texto VARCHAR2 (50);
+
+BEGIN
+    IF INSERTING THEN
+        texto := 'Inserção';
+    ELSIF UPDATING THEN
+        texto := 'Atualização';
+    ELSE
+        texto := 'Remoção';
+    END IF;
+
+    INSERT INTO AuditoriaLote (id, usuario, data_alteracao, operacao)
+    VALUES (id_audit.NEXTVAL, USER, SYSTIMESTAMP, texto);
+END;
